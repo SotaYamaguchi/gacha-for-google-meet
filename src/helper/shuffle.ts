8cadaa37e8cdba67469c03af619320c15ac91586
sota.yamaguchi <sota.yamaguchi@roxx.co.jp>
2022-02-01 19:01:01 +0900

    fix: shuffle 関数に lodash を使うように

--- src/helper/shuffle.ts
+++ src/helper/shuffle.ts
@@ -1,14 +1,7 @@
+import lodashShuffle from 'lodash/shuffle';
+
 type Shuffle = (list: string[]) => string[];
 
-const shuffle: Shuffle = (list) => {
-  const array = list.slice();
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    const temp = array[i];
-    list[i] = array[j];
-    list[j] = temp;
-  }
-  return list;
-};
+const shuffle: Shuffle = (list) => lodashShuffle(list);
 
 export default shuffle;
