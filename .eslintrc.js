9f59677ea037fc3902699decf96eddc6826101df
sota.yamaguchi <sota.yamaguchi@roxx.co.jp>
2022-07-23 17:15:29 +0900

    chore: fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,101 +1,117 @@
 module.exports = {
   env: {
     browser: true,
-    webextensions: true,
     commonjs: true,
-    es2021: true
+    es2021: true,
+    webextensions: true,
   },
-  plugins: [
-    'react',
-    'import',
-    'unused-imports',
-    '@typescript-eslint',
-    'sort-keys-fix',
-    'typescript-sort-keys',
-    'unicorn',
-  ],
   extends: [
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/errors',
-    'plugin:import/typescript',
-    'plugin:unicorn/recommended',
-    'standard',
-    'prettier',
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/errors",
+    "plugin:import/typescript",
+    "plugin:unicorn/recommended",
+    "standard",
+    "prettier",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
-    ecmaVersion: 'latest'
+    ecmaVersion: "latest",
+  },
+  plugins: [
+    "react",
+    "import",
+    "unused-imports",
+    "@typescript-eslint",
+    "sort-keys-fix",
+    "typescript-sort-keys",
+    "unicorn",
+  ],
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "error",
+    "@typescript-eslint/no-unused-vars": "error",
+    "import/no-cycle": "error",
+    "import/order": [
+      "error",
+      {
+        alphabetize: {
+          order: "asc",
+        },
+        groups: [
+          "builtin",
+          "external",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        "newlines-between": "never",
+      },
+    ],
+    "no-console": "warn",
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "sort-keys": ["error", "asc", { natural: true }],
+    "sort-keys-fix/sort-keys-fix": "error",
+    "typescript-sort-keys/interface": ["error", "asc", { natural: true }],
+    "typescript-sort-keys/string-enum": ["error", "asc", { natural: true }],
+    "unicorn/filename-case": "off",
+    "unicorn/prevent-abbreviations": "off",
+    "unused-imports/no-unused-imports": "error",
   },
   settings: {
-    "react": {
-      "createClass": "createReactClass", // Regex for Component Factory to use,
-                                         // default to "createReactClass"
-      "pragma": "React",  // Pragma to use, default to "React"
-      "fragment": "Fragment",  // Fragment to use (may be a property of <pragma>), default to "Fragment"
-      "version": "detect", // React version. "detect" automatically picks the version you have installed.
-                           // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
-                           // It will default to "latest" and warn if missing, and to "detect" in the future
-      "flowVersion": "0.53" // Flow version
-    },
-    "propWrapperFunctions": [
-      // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
-      "forbidExtraProps",
-      {"property": "freeze", "object": "Object"},
-      {"property": "myFavoriteWrapper"},
-      // for rules that check exact prop wrappers
-      {"property": "forbidExtraProps", "exact": true}
-    ],
-    "componentWrapperFunctions": [
+    componentWrapperFunctions: [
       // The name of any function used to wrap components, e.g. Mobx `observer` function. If this isn't set, components wrapped by these functions will be skipped.
       "observer", // `property`
-      {"property": "styled"}, // `object` is optional
-      {"property": "observer", "object": "Mobx"},
-      {"property": "observer", "object": "<pragma>"} // sets `object` to whatever value `settings.react.pragma` is set to
+      { property: "styled" }, // `object` is optional
+      { object: "Mobx", property: "observer" },
+      { object: "<pragma>", property: "observer" }, // sets `object` to whatever value `settings.react.pragma` is set to
     ],
-    "formComponents": [
+    formComponents: [
       // Components used as alternatives to <form> for forms, eg. <Form endpoint={ url } />
       "CustomForm",
-      {"name": "Form", "formAttribute": "endpoint"}
+      { formAttribute: "endpoint", name: "Form" },
     ],
-    "linkComponents": [
-      // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
-      "Hyperlink",
-      {"name": "Link", "linkAttribute": "to"}
-    ],
-    'import/resolver': {
+    "import/resolver": {
       typescript: {
         alwaysTryTypes: true,
       },
     },
-  },
-  rules: {
-    'no-console': 'warn',
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error",
-    'sort-keys-fix/sort-keys-fix': 'error',
-    'sort-keys': ['error', 'asc', { natural: true }],
-    'typescript-sort-keys/interface': ['error', 'asc', { natural: true }],
-    'typescript-sort-keys/string-enum': ['error', 'asc', { natural: true }],
-    'import/order': [
-      'error',
-      {
-        groups: ['builtin', 'external', 'parent', 'sibling', 'index', 'object', 'type'],
-        alphabetize: {
-          order: 'asc',
-        },
-        'newlines-between': 'never',
-      },
+    linkComponents: [
+      // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
+      "Hyperlink",
+      { linkAttribute: "to", name: "Link" },
     ],
-    'import/no-cycle': 'error',
-    'unused-imports/no-unused-imports': 'error',
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/explicit-function-return-type': 'error',
-    'unicorn/prevent-abbreviations': 'off',
-    'unicorn/filename-case': 'off',
-  }
-}
+    propWrapperFunctions: [
+      // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
+      "forbidExtraProps",
+      { object: "Object", property: "freeze" },
+      { property: "myFavoriteWrapper" },
+      // for rules that check exact prop wrappers
+      { exact: true, property: "forbidExtraProps" },
+    ],
+    react: {
+      createClass: "createReactClass",
+
+      // React version. "detect" automatically picks the version you have installed.
+      // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+      // It will default to "latest" and warn if missing, and to "detect" in the future
+      flowVersion: "0.53",
+
+      // Pragma to use, default to "React"
+      fragment: "Fragment",
+
+      // Regex for Component Factory to use,
+      // default to "createReactClass"
+      pragma: "React",
+
+      // Fragment to use (may be a property of <pragma>), default to "Fragment"
+      version: "detect", // Flow version
+    },
+  },
+};
